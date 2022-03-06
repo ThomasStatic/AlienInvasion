c3a5ddcc4d667b9b7f25b9502c93ef3b52064d88
Thomas Boyle <90807000+ThomasStatic@users.noreply.github.com>
2022-03-05 21:59:42 -0500

    Add files via upload

--- settings.py
+++ settings.py
@@ -7,7 +7,14 @@ class Settings:
 		# Screen settings
 		self.screen_width = 1200
 		self.screen_height = 600
-		self.bg_colour = (230, 230, 230)
+		self.bg_colour = (75, 0, 130)
 
 		# Ship settings
 		self.ship_speed = 0.85
+
+		# Bullet settings
+		self.bullet_speed = 1.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_colour = (250, 0, 0) # (red)
+		self.bullets_allowed = 3
